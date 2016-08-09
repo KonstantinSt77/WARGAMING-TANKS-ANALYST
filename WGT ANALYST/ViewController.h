21ecbcd940aa37dc46cd4bd4c1ae245a7b79d17e
Konstantin Stolyarenko <stolyarenko.kons@mail.ru>
2016-08-09 20:48:32 +0300

    Initial Commit

---
+++ WGT ANALYST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGT ANALYST
+//
+//  Created by Kostya on 09.08.16.
+//  Copyright © 2016 Stolyarenko K.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WGT ANALYST/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WGT ANALYST
+//
+//  Created by Kostya on 09.08.16.
+//  Copyright © 2016 Stolyarenko K.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
